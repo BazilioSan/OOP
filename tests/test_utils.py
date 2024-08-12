8fd133e7b724f3e24ab6e4f2a74379a3ad6945db
Alexandr <blix@list.ru>
2024-08-12 23:04:15 +0300

    add tests utils.py add config.py

---
+++ tests/test_utils.py
@@ -0,0 +1,37 @@
+from unittest.mock import patch
+
+import pytest
+
+from src.utils import get_data_from_json
+
+
+@pytest.fixture
+def json_data():
+    return [
+        {
+            "name": "Смартфоны",
+            "description": "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+            "products": [
+                {
+                    "name": "Samsung Galaxy C23 Ultra",
+                    "description": "256GB, Серый цвет, 200MP камера",
+                    "price": 180000.0,
+                    "quantity": 5,
+                },
+                {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
+                {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14},
+            ],
+        },
+        {
+            "name": "Телевизоры",
+            "description": "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+            "products": [
+                {"name": '55" QLED 4K', "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}
+            ],
+        },
+    ]
+
+@patch("json.load")
+def test_data_from_json(mock_json_load, json_data):
+    mock_json_load.return_value = json_data
+    assert get_data_from_json("products.json") == json_data
